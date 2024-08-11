3b9ad74b8f7dd986a72468daf8be215600c45665
Arnaout04 <hashemproductionz@gmail.com>
2024-08-11 22:54:25 +0300

    Started learning basics of python

---
+++ beginner_practice/python_basics.py
@@ -0,0 +1,51 @@
+#Python Basics
+print('Hello World!')
+
+#Variables
+x = 5
+y = "Hello, my name is Slim Shady"
+z = 13.6
+g = True
+
+#To print variables is simple
+
+print(x)
+print(y, "\n")
+print(z, "\n")
+print(g, "\n")
+
+#You can also specify
+
+t = float(5)
+
+#Can also acquire data type through type()
+
+print(type(t))
+
+#Single or double quotes are the same in python
+
+print('Hello')
+print("Heya")
+
+#Assigning multiple variables in single line
+
+h, l, p = 4 , 6 , 9
+
+ten = nine = seven = 'number'
+
+#Unpacking array
+
+fruits = ['apple', 'banana', 'orange']
+
+one, two, three = fruits
+
+#print variable styles
+
+print(one, two, three)
+
+print(one + two + three)
+
+#For numbers plus becomes addition
+
+print(h + p)
+
